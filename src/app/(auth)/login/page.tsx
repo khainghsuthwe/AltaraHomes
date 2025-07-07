41b95cb11876932e1e0f359dc8b7f0ed24ff3d41
khainghsuthwe <khaingjar@gmail.com>
2025-07-07 11:16:12 +0630

    [fix] fixed quoting error in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -82,7 +82,7 @@ export default function Login() {
           </button>
         </form>
         <p className="mt-4 text-center text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/register" className="text-blue-600 hover:text-blue-800">
             Sign Up
           </Link>
